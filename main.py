14d08d74deddbeb059031d1e05c826249ea0f6c7
wenhanhu <56165994+HWH88340@users.noreply.github.com>
2023-04-03 15:35:10 +0000

    ViT

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ from VGG16 import VGG16
 from VGG19 import VGG19
 from preprocess import preprocess
 from ResNet import ResNet
+from ViT import ViT
 
-# preprocess()
-# VGG16()
-# VGG19()
-ResNet()
+if __name__ == "__main__":
+    # preprocess()
+    # VGG16()
+    # VGG19()
+    # ResNet()
+    ViT()
