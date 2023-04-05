7006521c64ce81e46fcd061b829c1e08b2de5f74
wenhanhu <56165994+HWH88340@users.noreply.github.com>
2023-04-05 18:46:52 +0000

    VGG16 plot

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ from VGG16 import VGG16
 from VGG19 import VGG19
 from preprocess import preprocess
 from ResNet import ResNet
-from ViT import ViT
+# from ViT import ViT
 
 if __name__ == "__main__":
     # preprocess()
-    # VGG16()
+    VGG16()
     # VGG19()
     # ResNet()
-    ViT()
+    # ViT()
